b27975e283c7bbfeec94c828a1d13445db1b4aad
urahiroshi <asj.hiroshi@gmail.com>
2014-12-06 18:17:53 +0900

    rename dbClient methods

--- models/dbClient.js
+++ models/dbClient.js
@@ -5,18 +5,6 @@ var dbClient = require('mongodb'),
     Q = require('q'),
     client = {};
 
-client._qConnect = function() {
-  var d = Q.defer();
-  dbClient.connect(url, function(err, db) {
-    if(err == null) {
-      d.resolve(db);
-    } else {
-      d.reject(new Error(err));
-    }
-  })
-  return d.promise;
-};
-
 client.qExec = function(table, dbExec) {
   var d = Q.defer();
   dbClient.connect(url, function(err, db) {
@@ -37,39 +25,19 @@ client.qExec = function(table, dbExec) {
 };
 
 // get exaple: client.qRead('test',{}).done(function(result){console.log(result);});
-client.qRead = function(table, queryMap) {
+client.read = function(table, queryMap) {
   return client.qExec(table, function(collection, onFinish){
     collection.find(queryMap).toArray(onFinish);
   });
 };
 
-client.read = function(table, queryMap, callback) {
-  dbClient.connect(url, function(err, db) {
-    var collection = db.collection(table);
-    collection.find(queryMap).toArray(function(err, result) {
-      db.close();
-      callback(err, result);
-    });
-  });
-};
-
-client.qCreate = function(table, insertMap) {
+client.create = function(table, insertMap) {
   return client.qExec(table, function(collection, onFinish) {
     collection.insert(insertMap, onFinish);
   });
 };
 
-client.create = function(table, insertMap, callback) {
-  dbClient.connect(url, function(err, db) {
-    var collection = db.collection(table);
-    collection.insert(insertMap, function(err, result) {
-      db.close();
-      callback(err, result);
-    });
-  });
-};
-
-client.qUpdate = function(table, queryMap, updateMap) {
+client.update = function(table, queryMap, updateMap) {
   return client.qExec(table, function(collection, onFinish) {
     collection.update(
       queryMap,
@@ -79,36 +47,9 @@ client.qUpdate = function(table, queryMap, updateMap) {
   });
 };
 
-client.update = function(table, queryMap, updateMap, callback) {
-  dbClient.connect(url, function(err, db) {
-    var collection = db.collection(table);
-    collection.update(
-      queryMap,
-      { $set: updateMap },
-      function(err, result) {
-        db.close();
-        callback(err, result);
-      }
-    );
-  });
-};
-
-client.qDelete = function(table, queryMap) {
+client.delete = function(table, queryMap) {
   return client.qExec(table, function(collection, onFinish) {
     collection.remove(queryMap, onFinish);
-  });
-};
-
-client.delete = function(table, queryMap, callback) {
-  dbClient.connect(url, function(err, db) {
-    var collection = db.collection(table);
-    collection.remove(
-      queryMap,
-      function(err, result) {
-        db.close();
-        callback(err, result);
-      }
-    );
   });
 };
 
